a3d15e78ff57f5afe839b5412a9ff3e8000f8b4e
何方 <he529564582@163.com>
2022-11-19 21:02:08 +0800

    add 门店销售额排名静态

--- src/views/dashboard/Sale/index.vue
+++ src/views/dashboard/Sale/index.vue
@@ -14,7 +14,7 @@
         <el-date-picker
           class="date"
           type="monthrange"
-          range-separator="--"
+          range-separator="-"
           start-placeholder="开始月份"
           end-placeholder="结束月份"
           size="mini"
@@ -23,20 +23,121 @@
     </div>
     <div>
       <el-row :gutter="10">
-        <el-col :span="16">1</el-col>
-        <el-col :span="8">2</el-col>
+        <el-col :span="19">
+          <div ref="charts" class="charts" />
+        </el-col>
+        <el-col :span="5" class="right">
+          <h3>门店销售额排名</h3>
+          <ul>
+            <li>
+              <span class="rad">1</span>
+              <span>肯德基</span>
+              <span class="rvalue">111111</span>
+            </li>
+            <li>
+              <span class="rad">2</span>
+              <span>肯德基</span>
+              <span class="rvalue">222222</span>
+            </li>
+            <li>
+              <span class="rad">3</span>
+              <span>肯德基</span>
+              <span class="rvalue">333333</span>
+            </li>
+            <li>
+              <span class="rad">4</span>
+              <span>肯德基</span>
+              <span class="rvalue">444444</span>
+            </li>
+            <li>
+              <span class="rad">5</span>
+              <span>肯德基</span>
+              <span class="rvalue">555555</span>
+            </li>
+            <li>
+              <span class="rad">6</span>
+              <span>肯德基</span>
+              <span class="rvalue">666666</span>
+            </li>
+            <li>
+              <span class="rad">7</span>
+              <span>肯德基</span>
+              <span class="rvalue">777777</span>
+            </li>
+          </ul>
+        </el-col>
       </el-row>
     </div>
   </el-card>
 </template>
 
 <script>
+import * as echarts from 'echarts'
 export default {
   name: '',
   data() {
     return {
       activeName: '销售额'
     }
+  },
+  mounted() {
+    const myCharts = echarts.init(this.$refs.charts)
+    myCharts.setOption({
+      title: {
+        text: '销售额趋势'
+      },
+      tooltip: {
+        trigger: 'axis',
+        axisPointer: {
+          type: 'shadow'
+        }
+      },
+      grid: {
+        left: '3%',
+        right: '4%',
+        bottom: '3%',
+        containLabel: true
+      },
+      xAxis: [
+        {
+          type: 'category',
+          data: [
+            'Jan',
+            'Feb',
+            'Mar',
+            'Apr',
+            'May',
+            'Jun',
+            'Jul',
+            'Aug',
+            'Sep',
+            'Oct',
+            'Nov',
+            'Dec'
+          ],
+          axisTick: {
+            alignWithLabel: true
+          }
+        }
+      ],
+      yAxis: [
+        {
+          type: 'value'
+        }
+      ],
+      series: [
+        {
+          name: 'Direct',
+          type: 'bar',
+          barWidth: '60%',
+          data: [10, 52, 200, 334, 390, 330, 220, 52, 200, 334, 390, 330],
+          showBackground: true,
+          backgroundStyle: {
+            color: 'rgba(180, 180, 180, 0.2)'
+          }
+        }
+      ]
+    })
   }
 }
 </script>
@@ -60,5 +161,32 @@ export default {
 }
 .right span {
   margin: 0 10px;
+}
+.charts {
+  width: 100%;
+  height: 300px;
+}
+ul {
+  width: 100%;
+  height: 300px;
+  list-style: none;
+  padding: 0;
+}
+ul li {
+  height: 8%;
+}
+
+.rad {
+  /* display: block; */
+  float: left;
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  background: black;
+  color: white;
+  text-align: center;
+}
+.rvalue {
+  float: right;
 }
 </style>
