1fc4a19aa6db896bf32fc743931e0c4186b4252b
何方 <he529564582@163.com>
2022-11-24 18:41:33 +0800

    lineCharts

--- src/views/dashboard/Observe/Search/LineCharts/index.vue
+++ src/views/dashboard/Observe/Search/LineCharts/index.vue
@@ -1,11 +1,100 @@
 <template>
-  <div>折线图</div>
+  <div>
+    <div class="header">
+      <span>搜索用户数</span>
+      <i class="el-icon-info" />
+    </div>
+    <div class="main">
+      <span class="main-title">1111</span>
+      <span>11.11</span>
+      <i class="el-icon-caret-top" />
+    </div>
+    <div class="footer">
+      <div ref="charts" class="charts" />
+    </div>
+  </div>
 </template>
 
 <script>
+import * as echarts from 'echarts'
 export default {
-  name: 'LineCharts'
+  name: 'LineCharts',
+  mounted() {
+    const lineCharts = echarts.init(this.$refs.charts)
+    lineCharts.setOption({
+      xAxis: {
+        show: false,
+        type: 'category'
+      },
+      yAxis: {
+        show: false
+        // type: 'category'
+      },
+      series: [
+        {
+          type: 'line',
+          data: [17, 4, 2, 5, 10, 9, 2, 13, 22, 1, 5, 2],
+          itemStyle: {
+            opacity: 0
+          },
+          lineStyle: {
+            color: 'purple'
+          },
+          areaStyle: {
+            color: {
+              type: 'linear',
+              x: 0,
+              y: 0,
+              x2: 0,
+              y2: 1,
+              colorStops: [
+                {
+                  offset: 0,
+                  color: 'purple' // 0% 处的颜色
+                },
+                {
+                  offset: 1,
+                  color: 'white' // 100% 处的颜色
+                }
+              ],
+              global: false // 缺省为 false
+            }
+          }
+        }
+      ],
+      grid: {
+        left: 0,
+        right: 0,
+        top: 0,
+        bottom: 0
+      },
+      tooltip: {
+        // show: true
+      }
+    })
+  }
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+.el-icon-info {
+  font-size: 18px;
+  color: gray;
+  padding-left: 8px;
+}
+.el-icon-caret-top {
+  color: green;
+  font-size: 20px;
+  padding-left: 8px;
+}
+.main {
+  margin-top: 10px;
+}
+.main-title {
+  margin-right: 20px;
+}
+.charts {
+  width: 100%;
+  height: 100%;
+}
+</style>
--- src/views/dashboard/Observe/Search/index.vue
+++ src/views/dashboard/Observe/Search/index.vue
@@ -3,7 +3,18 @@
     <div slot="header" class="header">
       <div class="search-header">
         <span>线上热门搜索</span>
-        <i class="el-icon-more" />
+        <el-dropdown>
+          <span class="el-dropdown-link">
+            <i class="el-icon-more" />
+          </span>
+          <el-dropdown-menu slot="dropdown">
+            <el-dropdown-item>黄金糕</el-dropdown-item>
+            <el-dropdown-item>狮子头</el-dropdown-item>
+            <el-dropdown-item>螺蛳粉</el-dropdown-item>
+            <el-dropdown-item>双皮奶</el-dropdown-item>
+            <el-dropdown-item>蚵仔煎</el-dropdown-item>
+          </el-dropdown-menu>
+        </el-dropdown>
       </div>
     </div>
     <div>
@@ -32,6 +43,7 @@ export default {
 <style scoped>
 .header {
   border-bottom: 1px solid #eee;
+  padding: 5px 0;
 }
 .search-header {
   display: flex;
