e620471b8a5d2b32f8b402eec9cfd0f40cd6c476
何方 <he529564582@163.com>
2022-11-26 22:22:30 +0800

    lineCharts

--- src/views/dashboard/Observe/Search/LineCharts/index.vue
+++ src/views/dashboard/Observe/Search/LineCharts/index.vue
@@ -33,7 +33,7 @@ export default {
       series: [
         {
           type: 'line',
-          data: [17, 4, 2, 5, 10, 9, 2, 13, 22, 1, 5],
+          data: [17, 4, 2, 5, 10, 9, 2, 13, 22, 1],
           itemStyle: {
             opacity: 0
           },
