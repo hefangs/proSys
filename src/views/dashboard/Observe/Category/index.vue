ea093f765db13b3231e7906945d3b6fe59b9d860
何方 <he529564582@163.com>
2022-12-02 21:58:30 +0800

    add

--- src/views/dashboard/Observe/Category/index.vue
+++ src/views/dashboard/Observe/Category/index.vue
@@ -28,7 +28,7 @@ export default {
     charts.setOption({
       title: {
         text: 'Search Eng',
-        subtext: 1048,
+        subtext: 104ß,
         left: 'center',
         top: 'center'
       },
